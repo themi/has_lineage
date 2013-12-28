ce7ecd5463028f8b3b3e9c014c1b3d816bb116ae
Tim Hemi <tim@reinteractive.net>
2013-12-28 13:37:04 +1100

    adding indexing

--- lib/generators/has_lineage/templates/migration.rb
+++ lib/generators/has_lineage/templates/migration.rb
@@ -5,4 +5,8 @@ class <%= migration_class_name %> < ActiveRecord::Migration
     add_column :<%= table_name %>, :parent_id, :integer
     add_column :<%= table_name %>, :branch_id, :integer
   end
+
+  add_index :<%= table_name %>, :lineage
+  add_index :<%= table_name %>, :parent_id
+  add_index :<%= table_name %>, :branch_id
 end
