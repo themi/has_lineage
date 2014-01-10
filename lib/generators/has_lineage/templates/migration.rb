3c40a80a0d7c06e9939dfa3266af721ec574a5c0
Tim Hemi <tim@reinteractive.net>
2014-01-10 20:20:06 +1100

    correcting migration file template

--- lib/generators/has_lineage/templates/migration.rb
+++ lib/generators/has_lineage/templates/migration.rb
@@ -1,12 +1,12 @@
 class <%= migration_class_name %> < ActiveRecord::Migration
   def change
-    add_column :<%= table_name %>, :linage, :string
-    add_column :<%= table_name %>, :lineage_children_count, :integer
-    add_column :<%= table_name %>, :parent_id, :integer
-    add_column :<%= table_name %>, :branch_id, :integer
-  end
+    add_column :<%= table_name %>, :lineage, :string
+    add_column :<%= table_name %>, :parent_id, :integer               # Remove if parent key already set
+    add_column :<%= table_name %>, :branch_id, :integer               # Remove if only one tree hierarchy needed
+    add_column :<%= table_name %>, :lineage_children_count, :integer  # Remove if cache_count not required
 
-  add_index :<%= table_name %>, :lineage
-  add_index :<%= table_name %>, :parent_id
-  add_index :<%= table_name %>, :branch_id
+    add_index :<%= table_name %>, :lineage
+    add_index :<%= table_name %>, :parent_id                          # Remove if parent key already set
+    add_index :<%= table_name %>, :branch_id                          # Remove if only one tree hierarchy needed
+  end
 end
